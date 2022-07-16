fe8cb00392ce9c6b8a184674597508b38dc1d8a2
Vagner Lourenço <90052483+VagnerSenac@users.noreply.github.com>
2022-07-16 11:39:29 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+usuario = input("Informe o seu usuário: ")
+senha = input("Informe sua senha: ")
+
+if usuario == 'joao' and senha == '123':
+    print("pode entrar")
+else:
+    print("usuário ou senha inválido")
