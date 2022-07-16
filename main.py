a88520c11ccc8479ea1e84aea5b62f5c8dd8e1d5
Vagner Lourenço <90052483+VagnerSenac@users.noreply.github.com>
2022-07-16 12:35:28 -0300

    usuário
    Criação de arquivo e controle de acesso

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-usuario = input("Informe o seu usuário: ")
-senha = input("Informe sua senha: ")
 
-if usuario == 'joao' and senha == '123':
+#View - o que vai pro usuário do sistema
+usuario_digitado = input("Informe o seu usuário: ")
+senha_digitado = input("Informe sua senha: ")
+
+#Model - O que vem do banco de dados (BD)
+usuario_BD = 'joao'
+senha_BD = '123'
+
+#Controller - a validação
+if usuario_digitado == usuario_BD and senha_digitado == senha_BD:
     print("pode entrar")
 else:
     print("usuário ou senha inválido")
